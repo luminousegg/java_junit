968b66e346b053804b243b1822d2fc33049a04dd
wangzhen <wangzhen1513@sina.com>
2019-08-26 17:23:07 +0800

    建立关系

---
+++ src/main/java/com/yiibai/OrderBo.java
@@ -0,0 +1,31 @@
+package com.yiibai;
+//
+///**
+// * Created by wangzhen on 2019-08-26
+// */
+//public class OrderBo {
+//    public void save(Order order) throws OrderSaveException {
+//
+//        if (order == null) {
+//            throw new OrderSaveException("Order is empty!");
+//        }
+//
+//        // persist it
+//
+//    }
+//
+//    public void update(Order order) throws OrderUpdateException, OrderNotFoundException {
+//
+//        if (order == null) {
+//            throw new OrderUpdateException("Order is empty!");
+//        }
+//
+//        // if order is not available in database
+//        throw new OrderNotFoundException("Order is not exists");
+//
+//    }
+//
+//}
+
+
+
---
+++ src/main/java/com/yiibai/RandomEmailGenerator.java
@@ -0,0 +1,12 @@
+package com.yiibai;
+
+/**
+ * Created by wangzhen on 2019-08-26
+ */
+public class RandomEmailGenerator {
+    public String generate() {
+        return "feedback@yiibai.com";
+    }
+
+
+}
